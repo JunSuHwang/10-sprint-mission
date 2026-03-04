0cc6c46c795d9f6cc82eff0b61f18c638d2c67bd
JunSuHwang <wnstn9904@naver.com>
2026-03-04 13:28:48 +0900

    feat: 테이블 스키마 파일 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,70 @@
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    created_at timestamptz         NOT NULL,
+    updated_at timestamptz,
+    username   varchar(50) unique  NOT NULL,
+    email      varchar(100) unique NOT NULL,
+    password   varchar(60)         NOT NULL,
+    profile_id UUID unique
+);
+CREATE TABLE channels
+(
+    id          UUID PRIMARY KEY,
+    created_at  timestamptz NOT NULL,
+    updated_at  timestamptz,
+    name        varchar(100),
+    description varchar(500),
+    type        varchar(10) check (type in ('PUBLIC', 'PRIVATE'))
+);
+CREATE TABLE messages
+(
+    id         UUID PRIMARY KEY,
+    created_at timestamptz NOT NULL,
+    updated_at timestamptz,
+    content    TEXT,
+    channel_id UUID        NOT NULL,
+    author_id  UUID,
+    FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE SET NULL
+);
+CREATE TABLE user_statuses
+(
+    id             UUID PRIMARY KEY,
+    created_at     timestamptz NOT NULL,
+    updated_at     timestamptz,
+    user_id        UUID unique NOT NULL,
+    last_active_at timestamptz NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE TABLE read_statuses
+(
+    id           UUID PRIMARY KEY,
+    created_at   timestamptz NOT NULL,
+    updated_at   timestamptz,
+    user_id      UUID        NOT NULL,
+    channel_id   UUID        NOT NULL,
+    last_read_at timestamptz NOT NULL,
+    UNIQUE (user_id, channel_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE
+);
+CREATE TABLE binary_contents
+(
+    id           UUID PRIMARY KEY,
+    created_at   timestamptz  NOT NULL,
+    filename     varchar(255) NOT NULL,
+    size         bigint       NOT NULL,
+    content_type varchar(100) NOT NULL,
+    bytes        bytea        NOT NULL
+);
+CREATE TABLE message_attachments
+(
+    message_id    UUID NOT NULL,
+    attachment_id UUID NOT NULL,
+    FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE,
+    FOREIGN KEY (attachment_id) REFERENCES binary_contents (id) ON DELETE CASCADE
+);
+ALTER TABLE users
+    ADD CONSTRAINT fk_users_binary_contents FOREIGN KEY (profile_id)
+        REFERENCES binary_contents (id) ON DELETE SET NULL;
