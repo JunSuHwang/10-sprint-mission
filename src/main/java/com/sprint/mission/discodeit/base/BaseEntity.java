765b44bc432055522d9b0b3c67008462326ba3c0
JunSuHwang <wnstn9904@naver.com>
2026-03-04 16:07:42 +0900

    feat: Base 엔티티에 Jpa 적용

--- src/main/java/com/sprint/mission/discodeit/base/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/base/BaseEntity.java
@@ -1,20 +1,32 @@
 package com.sprint.mission.discodeit.base;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import java.time.Instant;
 import java.util.UUID;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class BaseEntity {
 
-  protected final UUID id;
-  @CreatedDate
-  protected Instant createdAt;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  protected UUID id;
 
-  public BaseEntity() {
-    this.id = UUID.randomUUID();
-  }
+  @CreatedDate
+  @Column(nullable = false, updatable = false)
+  protected Instant createdAt;
 
   @Override
   public boolean equals(Object obj) {
--- src/main/java/com/sprint/mission/discodeit/base/BaseUpdatableEntity.java
+++ src/main/java/com/sprint/mission/discodeit/base/BaseUpdatableEntity.java
@@ -1,10 +1,14 @@
 package com.sprint.mission.discodeit.base;
 
+import jakarta.persistence.MappedSuperclass;
 import java.time.Instant;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.LastModifiedDate;
 
 @Getter
+@MappedSuperclass
+@NoArgsConstructor
 public abstract class BaseUpdatableEntity extends BaseEntity {
 
   @LastModifiedDate
