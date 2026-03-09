7caf8566a0da2ad2705e3e605cfb664123d4eb34
JunSuHwang <wnstn9904@naver.com>
2026-03-09 18:49:46 +0900

    refactor: channel의 메세지 목록 가져오는 엔드포인트 병합

--- src/main/java/com/sprint/mission/discodeit/message/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/message/controller/MessageController.java
@@ -8,6 +8,7 @@ import com.sprint.mission.discodeit.message.dto.MessageUpdateRequest;
 import com.sprint.mission.discodeit.message.service.MessageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Encoding;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
@@ -30,6 +31,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -141,5 +144,22 @@ public class MessageController {
   ) {
     messageService.deleteMessage(messageId);
     return ResponseEntity.noContent().build();
+  }
+
+  @Operation(summary = "Channel의 Message 목록 조회")
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200", description = "Message 목록 조회 성공",
+          content = @Content(
+              mediaType = MediaType.ALL_VALUE,
+              array = @ArraySchema(schema = @Schema(implementation = MessageDto.class))
+          )
+      )
+  })
+  @RequestMapping(method = RequestMethod.GET)
+  public ResponseEntity<List<MessageDto>> findAllByChannelId(
+      @Parameter(description = "조회할 Channel ID") @RequestParam UUID channelId
+  ) {
+    return ResponseEntity.ok(messageService.findAllByChannelId(channelId));
   }
 }
