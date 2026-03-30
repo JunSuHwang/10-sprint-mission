3093a01a85ede2365bb1a8d23e9511f9e49f3c44
JunSuHwang <wnstn9904@naver.com>
2026-03-30 14:14:25 +0900

    feat: 요청 dto 필드에 validation 추가

--- src/main/java/com/sprint/mission/discodeit/channel/dto/PrivateChannelCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/channel/dto/PrivateChannelCreateRequest.java
@@ -1,11 +1,13 @@
 package com.sprint.mission.discodeit.channel.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import java.util.UUID;
 
 @Schema(description = "Private Channel 생성 정보")
 public record PrivateChannelCreateRequest(
+    @Size(min = 2, message = "참가 유저는 최소 2명이어야 합니다.")
     List<UUID> participantIds
 ) {
 
--- src/main/java/com/sprint/mission/discodeit/userstatus/dto/UserStatusCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/userstatus/dto/UserStatusCreateRequest.java
@@ -1,10 +1,16 @@
 package com.sprint.mission.discodeit.userstatus.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import java.time.Instant;
 import java.util.UUID;
 
 public record UserStatusCreateRequest(
+    @NotNull
     UUID userId,
+
+    @NotNull
+    @PastOrPresent(message = "마지막 접속 날짜는 현재보다 이전이어야 합니다.")
     Instant lastActiveAt
 ) {
 
