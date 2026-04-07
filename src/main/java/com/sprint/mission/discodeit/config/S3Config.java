bbd7cfde034982a58c17cb4c446176dd7607c4e4
JunSuHwang <wnstn9904@naver.com>
2026-04-07 14:35:20 +0900

    feat: s3 테스트클래스 추가

--- src/main/java/com/sprint/mission/discodeit/config/S3Config.java
+++ src/main/java/com/sprint/mission/discodeit/config/S3Config.java
@@ -7,6 +7,7 @@ import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
@@ -36,6 +37,23 @@ public class S3Config {
     return S3Client.builder()
         .region(Region.of(props.getRegion()))
         .credentialsProvider(DefaultCredentialsProvider.create())
+        .build();
+  }
+
+  @Bean
+  public S3Presigner s3Presigner() {
+    return S3Presigner.builder()
+        .region(Region.of(props.getRegion()))
+        .credentialsProvider(
+            (props.getCredentials().getAccessKey() != null
+                && !props.getCredentials().getSecretKey().isBlank())
+                ? StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(
+                    props.getCredentials().getAccessKey(),
+                    props.getCredentials().getSecretKey()
+                )
+            ) : DefaultCredentialsProvider.create()
+        )
         .build();
   }
 }
