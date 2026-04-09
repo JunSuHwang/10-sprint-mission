315b544c7f9016afc5d6309d19f19b016eaf0387
JunSuHwang <wnstn9904@naver.com>
2026-04-09 09:36:25 +0900

    fix: userStatus 시간 업데이트가 서버 기준으로 동작하게 수정

--- src/main/java/com/sprint/mission/discodeit/userstatus/dto/UserStatusUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/userstatus/dto/UserStatusUpdateRequest.java
@@ -1,13 +1,10 @@
 package com.sprint.mission.discodeit.userstatus.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import java.time.Instant;
 
 @Schema(description = "변경할 User 온라인 상태 정보")
 public record UserStatusUpdateRequest(
-    @PastOrPresent(message = "마지막 접속 날짜는 현재보다 이전이어야 합니다.")
     Instant newLastActiveAt
 ) {
 
