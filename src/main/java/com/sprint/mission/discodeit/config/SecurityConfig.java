259052963e0576437434218b517d725076f5f083
JunSuHwang <wnstn9904@naver.com>
2026-05-18 10:02:10 +0900

    feat: /me, 로그아웃 설정 추가

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -5,10 +5,12 @@ import com.sprint.mission.discodeit.security.LoginSuccessHandler;
 import com.sprint.mission.discodeit.security.SpaCsrfTokenRequestHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -21,10 +23,19 @@ public class SecurityConfig {
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
         )
+        .authorizeHttpRequests(auth -> auth
+            .anyRequest().permitAll()
+        )
         .formLogin(login -> login
             .loginProcessingUrl("/api/auth/login")
             .successHandler(new LoginSuccessHandler())
             .failureHandler(new LoginFailureHandler())
+        )
+        .logout(
+            logout -> logout
+                .logoutUrl("/api/auth/logout")
+                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(
+                    HttpStatus.NO_CONTENT))
         )
         .build();
   }
--- src/main/java/com/sprint/mission/discodeit/user/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/user/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.sprint.mission.discodeit.user.controller;
 
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
+import com.sprint.mission.discodeit.user.dto.UserDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @Tag(name = "Auth", description = "인증 API")
 public class AuthController {
-  
+
   @GetMapping("/csrf-token")
   public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
     String tokenValue = csrfToken.getToken();
@@ -24,5 +27,16 @@ public class AuthController {
         .status(203)
         .header("X-XSRF-TOKEN", tokenValue)
         .build();
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity<UserDto> authMe(@AuthenticationPrincipal DiscodeitUserDetails userDetails) {
+    if (userDetails == null) {
+      return ResponseEntity.status(401).build();
+    }
+    log.info("[auth/me] userDto name: {}, email: {}, profile: {}",
+        userDetails.getUserDto().username(), userDetails.getUserDto().email(),
+        userDetails.getUserDto().profile());
+    return ResponseEntity.ok(userDetails.getUserDto());
   }
 }
